a8d3580b7f853b1ab1b4161c8e9764ef94571343
Josef Holzmayr <jester@theyoctojester.info>
2020-11-23 16:48:55 +0100

    make boost usage configuratble

--- answer.cpp
+++ answer.cpp
@@ -4,10 +4,17 @@
 #include <stdlib.h>
 
 #include <string>
+#ifdef USEBOOST
 #include <boost/lexical_cast.hpp>
+#endif
 
 void answer() {
+#ifdef USEBOOST
 	std::string call = "echo \"" + boost::lexical_cast<std::string>(6) + " * " + boost::lexical_cast<std::string>(7) + "\" | bc";
+#else
+	std::string call = "echo \"6 * 7\" | bc";
+#endif
+
 	printf("the answer is\n");
 	system(call.c_str());
 }
