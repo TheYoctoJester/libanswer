f1664061a78efcf1322277af119982c07bf9eb46
Josef Holzmayr <jester@theyoctojester.info>
2020-11-24 10:36:17 +0100

    make boost and verbosity optional

--- answer.cpp
+++ answer.cpp
@@ -11,10 +11,15 @@
 void answer() {
 #ifdef USEBOOST
 	std::string call = "echo \"" + boost::lexical_cast<std::string>(6) + " * " + boost::lexical_cast<std::string>(7) + "\" | bc";
+	printf("we are using boost!\n");
 #else
 	std::string call = "echo \"6 * 7\" | bc";
 #endif
 
 	printf("the answer is\n");
+
+#if VERBOSEOUTPUT
+	printf("(using calculation string: %s)\n", call.c_str());
+#endif
 	system(call.c_str());
 }
